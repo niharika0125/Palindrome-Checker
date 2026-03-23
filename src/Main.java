9be0176cba7c4d70aad8f4e282f1e8dc328e00b1
Niharika <kurabalakotaniharika@gmail.com>
2026-03-23 23:11:55 +0530

    UC11: OOP based Palindrome Service

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String word = "Madam";
+        String word = "madam";
 
-        // convert to lowercase
-        String normalized = word.toLowerCase();
+        PalindromeService service = new PalindromeService();
 
-        String reversed = "";
-
-        // reverse string
-        for (int i = normalized.length() - 1; i >= 0; i--) {
-            reversed += normalized.charAt(i);
-        }
-
-        if (normalized.equals(reversed)) {
-            System.out.println(word + " is a Palindrome (Case-Insensitive)");
+        if (service.isPalindrome(word)) {
+            System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
         }
