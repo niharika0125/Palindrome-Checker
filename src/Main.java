ec0282497f8e82eaaa8b1aa406b81f308f387a43
Niharika <kurabalakotaniharika@gmail.com>
2026-03-23 22:55:27 +0530

    UC2: Hardcoded Palindrome Check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker Application!");
+        String word = "madam";
+
+        if (word.equals("madam")) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
     }
 }
